e78eeda266e9d49df1a3fa99d681d43b831a6d12
Nils Homer <nilshomer@gmail.com>
2022-04-20 16:53:30 -0700

    cleanup

--- src/main/scala/com/fulcrumgenomics/util/Metric.scala
+++ src/main/scala/com/fulcrumgenomics/util/Metric.scala
@@ -25,24 +25,21 @@
 
 package com.fulcrumgenomics.util
 
-import com.fulcrumgenomics.cmdline.FgBioMain.FailureException
-import com.fulcrumgenomics.commons.CommonsDef._
 import com.fulcrumgenomics.commons.io.{Writer => CommonsWriter}
 import com.fulcrumgenomics.commons.reflect.{ReflectionUtil, ReflectiveBuilder}
-import com.fulcrumgenomics.commons.util.{DelimitedDataParser, LazyLogging}
+import com.fulcrumgenomics.commons.util.DelimitedDataParser
 import enumeratum.EnumEntry
 import htsjdk.samtools.util.Iso8601Date
 
-import java.io.{PrintWriter, StringWriter, Writer}
+import java.io.Writer
 import java.nio.file.Path
 import java.text.{DecimalFormat, NumberFormat, SimpleDateFormat}
 import java.util.Date
 import scala.collection.compat._
 import scala.collection.concurrent.TrieMap
 import scala.reflect.runtime.{universe => ru}
-import scala.util.{Failure, Success}
 
-object Metric extends LazyLogging {
+object Metric {
   val Delimiter: Char = '\t'
   val DelimiterAsString: String = s"$Delimiter"
 
@@ -99,76 +96,17 @@ object Metric extends LazyLogging {
     }
   }
 
-  private[util] def build[T <: Metric](reflectiveBuilder: ReflectiveBuilder[T],
-                                       toArg: Int => String,
-                                       argNames: Option[Seq[String]] = None,
-                                       fail: (String, Option[Throwable]) => Unit
-                                      )(implicit tt: ru.TypeTag[T]): T = {
-    val names = argNames.getOrElse(Metric.names[T])
-    forloop(from = 0, until = names.length) { i =>
-      reflectiveBuilder.argumentLookup.forField(names(i)) match {
-        case Some(arg) =>
-          val value = {
-            val tmp = toArg(i)
-            if (tmp.isEmpty && arg.argumentType == classOf[Option[_]]) ReflectionUtil.SpecialEmptyOrNoneToken else tmp
-          }
-
-          val argumentValue = ReflectionUtil.constructFromString(arg.argumentType, arg.unitType, value) match {
-            case Success(v) => v
-            case Failure(thr) =>
-              fail(s"Could not construct value for column '${arg.name}' of type '${arg.typeDescription}' from '$value'", Some(thr))
-          }
-          arg.value = argumentValue
-        case None =>
-          fail(s"Did not have a field with name '${names(i)}'.", None)
-      }
-    }
-
-    // build it.  NB: if arguments are missing values, then an exception will be thrown here
-    // Also, we don't use the default "build()" method since if a collection or option is empty, it will be treated as
-    // missing.
-    reflectiveBuilder.build(reflectiveBuilder.argumentLookup.ordered.map(arg => arg.value getOrElse unreachable(s"Arguments not set: ${arg.name}")))
-  }
-
-  private[util] def failReading[T <: Metric](clazz: Class[T],
-                                             message: String,
-                                             lineNumber: Option[Int] = None,
-                                             throwable: Option[Throwable] = None,
-                                             source: Option[String] = None): Unit = {
-    val sourceMessage = source.map("\nIn source: " + _).getOrElse("")
-    val prefix        = lineNumber match {
-      case None    => "For metric"
-      case Some(n) => s"On line #$n for metric"
-    }
-    val fullMessage   = s"$prefix '${clazz.getSimpleName}'$sourceMessage\n$message"
-    throwable.foreach { thr =>
-      val stringWriter = new StringWriter
-      thr.printStackTrace(new PrintWriter(stringWriter))
-      val banner = "#" * 80
-      logger.debug(banner)
-      logger.debug(stringWriter.toString)
-      logger.debug(banner)
-    }
-    throw FailureException(message=Some(fullMessage))
-  }
-
   /** Reads metrics from a set of lines.  The first line should be the header with the field names.  Each subsequent
     * line should be a single metric. */
   def iterator[T <: Metric](lines: Iterator[String], source: Option[String] = None)(implicit tt: ru.TypeTag[T]): Iterator[T] = {
-    val clazz: Class[T]  = ReflectionUtil.typeTagToClass[T]
-
-    if (lines.isEmpty) failReading(clazz=clazz, message="No header found", lineNumber=Some(1), source=source)
-    val parser            = new DelimitedDataParser(lines=lines, delimiter=Delimiter, ignoreBlankLines=false, trimFields=true)
-    val names             = parser.headers.toIndexedSeq
-    val reflectiveBuilder = new ReflectiveBuilder(clazz)
+    val builder = new MetricBuilder[T](source=source)(tt)
+    if (lines.isEmpty) builder.fail(message="No header found", lineNumber=Some(1))
+    val parser  = new DelimitedDataParser(lines=lines, delimiter=Delimiter, ignoreBlankLines=false, trimFields=true)
+    val names   = parser.headers.toIndexedSeq
 
     parser.zipWithIndex.map { case (row, rowIndex) =>
-      build(
-        reflectiveBuilder = reflectiveBuilder,
-        toArg             = i => row[String](i),
-        argNames          = Some(names),
-        fail              = (message, throwable) => failReading(clazz=clazz, message=message, lineNumber=Some(rowIndex+2), throwable=throwable, source=source)
-      )
+      val argMap = names.zipWithIndex.map { case (name, i) => name -> row[String](i) }.toMap
+      builder.build(argMap=argMap, lineNumber=Some(rowIndex+2))
     }
   }
 
@@ -225,40 +163,6 @@ object Metric extends LazyLogging {
   def writer[T <: Metric](writer: Writer)(implicit tt: ru.TypeTag[T]): MetricWriter[T] = new MetricWriter[T](writer)
 }
 
-
-class MetricSorter[Key <: Ordered[Key], T <: Metric](maxObjectsInRam: Int = MetricSorter.MaxInMemory,
-                                                     keyfunc: T => Key,
-                                                     tmpDir: DirPath = Io.tmpDir
-                                                    )(implicit tt: ru.TypeTag[T]) extends Sorter[T, Key](
-  maxObjectsInRam = maxObjectsInRam,
-  codec           = new MetricSorter.MetricSorterCodec[T](),
-  keyfunc         = keyfunc,
-  tmpDir          = tmpDir
-)
-
-object MetricSorter {
-  /** The default maximum # of records to keep and sort in memory. */
-  val MaxInMemory: Int = 1e6.toInt
-
-  class MetricSorterCodec[T <: Metric]()(implicit tt: ru.TypeTag[T])
-    extends Sorter.Codec[T] with LazyLogging {
-    private val clazz: Class[T]   = ReflectionUtil.typeTagToClass[T]
-    private val reflectiveBuilder = new ReflectiveBuilder(clazz)
-
-    /** Encode the metric into an array of bytes. */
-    def encode(metric: T): Array[Byte] = metric.values.mkString(Metric.DelimiterAsString).getBytes
-
-    /** Decode a metric from an array of bytes. */
-    def decode(bs: Array[Byte], start: Int, length: Int): T = {
-      val fields = new String(bs.slice(from=start, until=start+length)).split(Metric.DelimiterAsString)
-      Metric.build(
-        reflectiveBuilder = reflectiveBuilder,
-        toArg = i => fields(i),
-        fail  = (message, throwable) => Metric.failReading(clazz=clazz, message=message, throwable=throwable)
-      )
-    }
-  }
-}
 
 /**
   * Base trait for metrics.
